96223362013d0583831754468ef5f96055f269b9
Gary Brown <gary@brownuk.com>
2017-06-14 01:05:44 +0100

    Add simple jar artifact to test auto build/release

---
+++ opentracing-metrics/src/main/java/io/opentracing/contrib/metrics/Metrics.java
@@ -0,0 +1,10 @@
+package io.opentracing.contrib.metrics;
+
+import io.opentracing.Tracer;
+
+public class Metrics {
+
+    public static Tracer decorate(Tracer tracer) {
+        return null;
+    }
+}
