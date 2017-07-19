e0e51b3a6a0ec6b921faecb4cdc627b7a0c0aee5
Kevin Earls <kearls@redhat.com>
2017-07-19 16:54:08 +0200

    Increase test coverage 3 (#14)
    Add tests to increase coverage of MetricsTracer

--- opentracing-metrics/src/test/java/io/opentracing/contrib/metrics/internal/tracer/MetricsTracerTest.java
+++ opentracing-metrics/src/test/java/io/opentracing/contrib/metrics/internal/tracer/MetricsTracerTest.java
@@ -14,6 +14,7 @@
 package io.opentracing.contrib.metrics.internal.tracer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
@@ -94,6 +95,94 @@ public class MetricsTracerTest {
         assertEquals("parent", captured.get(1).getOperationName());
         assertEquals(tracer.finishedSpans().get(0).tags(), captured.get(0).getTags());
         assertEquals(tracer.finishedSpans().get(1).tags(), captured.get(1).getTags());
+    }
+
+    @Test
+    public void testWithTags() {
+        MetricsReporter reporter = Mockito.mock(MetricsReporter.class);
+        MockTracer tracer = new MockTracer(new ThreadLocalActiveSpanSource());
+        Tracer metricsTracer = Metrics.decorate(tracer, reporter);
+
+        ActiveSpan parent = metricsTracer.buildSpan("parent")
+                .withTag("booleanTag", true)
+                .withTag("numericTag", new Integer(100))
+                .startActive();
+
+        parent.deactivate();
+
+        List<MockSpan> spans = tracer.finishedSpans();
+        assertEquals(1, spans.size());
+        MockSpan span = spans.get(0);
+        Map<String, Object> tags = span.tags();
+
+        Object booleanTag = tags.get("booleanTag");
+        assertNotNull("Expected a tag named 'booleanTag'", booleanTag);
+        assertTrue("booleanTag should be a Boolean", booleanTag instanceof Boolean);
+        assertEquals("booleanTag should be true", true, (Boolean) booleanTag);
+
+        Object numericTag = tags.get("numericTag");
+        assertNotNull("Expected a tag named 'numericTag'", numericTag);
+        assertTrue("numericTag should be a Number", numericTag instanceof Number);
+        assertEquals("numericTag should be 100", 100, (Number) numericTag);
+    }
+
+    @Test
+    public void testWithStartTimestamp() throws InterruptedException {
+        MetricsReporter reporter = Mockito.mock(MetricsReporter.class);
+        MockTracer tracer = new MockTracer(new ThreadLocalActiveSpanSource());
+        Tracer metricsTracer = Metrics.decorate(tracer, reporter);
+
+        long start = System.currentTimeMillis() * 687;
+        Thread.sleep(100);
+        ActiveSpan parent = metricsTracer.buildSpan("parent")
+                .withStartTimestamp(start)
+                .startActive();
+
+        parent.deactivate();
+
+        List<MockSpan> spans = tracer.finishedSpans();
+        assertEquals(1, spans.size());
+        MockSpan span = spans.get(0);
+        long started = span.startMicros();
+        assertEquals(start, started);
+    }
+
+    @Test
+    public void testAsChildOf() {
+        MetricsReporter reporter = Mockito.mock(MetricsReporter.class);
+        MockTracer tracer = new MockTracer(new ThreadLocalActiveSpanSource());
+        Tracer metricsTracer = Metrics.decorate(tracer, reporter);
+
+        ActiveSpan parentSpan = metricsTracer.buildSpan("parent")
+                .withTag("spanName","parent")
+                .startActive();
+        parentSpan.setTag("additionalTag", "parent");
+
+        ActiveSpan childSpan = metricsTracer.buildSpan("child")
+                .asChildOf(parentSpan)
+                .withTag("spanName","child")
+                .startActive();
+        childSpan.setTag("additionalTag", "child");
+
+        childSpan.deactivate();
+        parentSpan.deactivate();
+
+        List<MockSpan> spans = tracer.finishedSpans();
+        assertEquals(2, spans.size());
+        MockSpan span1 = spans.get(0);
+        MockSpan span2 = spans.get(1);
+
+        MockSpan parent, child;
+        if (span1.operationName().equals("parent")) {
+            parent = span1;
+            child = span2;
+        } else {
+            parent = span2;
+            child = span1;
+        }
+
+        assertEquals("Child's parent id should be spanId of parent", child.parentId(), parent.context().spanId());
+        assertEquals(0, parent.parentId());
     }
 
 }
